ca6483a46503d9e283e8addf56c09517678791b0
sa <steinar.a.volden@gmail.com>
2020-09-11 22:50:00 +0300

    new menu building algorithm, cascade corrections

--- tree_menu/models.py
+++ tree_menu/models.py
@@ -16,8 +16,23 @@ class MenuNode(models.Model):
         verbose_name = 'Пункт меню'
         verbose_name_plural = 'Пункты меню'
 
+    def cascade_node_correction(self, child):
+        child.save()
+        for next_child in child.menunode_set.all():
+            self.cascade_node_correction(next_child)
+
     def save(self, *args, **kwargs):
         if self.parent:
             self.level = self.parent.level + 1
             self.menu_name = self.parent.menu_name  # I don't want children to be separated from their parents!
+        else:
+            self.level = 0
+        for child in self.menunode_set.all():
+            self.cascade_node_correction(child)
         super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        children = list(self.menunode_set.all())
+        super().delete(*args, **kwargs)
+        for child in children:
+            self.cascade_node_correction(child)
--- tree_menu/templatetags/tree_menu.py
+++ tree_menu/templatetags/tree_menu.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db import connection
 from tree_menu.models import MenuNode
 
 register = template.Library()
@@ -9,24 +10,44 @@ def draw_menu(menu_name, path):
     nodes = list(MenuNode.objects.filter(menu_name=menu_name))
     active_node = None
     path = path.strip('/')
-    for node in nodes:
+    menu = []
+    nodelists = []
+    # TODO: This whole algorithm is too clumsy and possibly needs to be rewritten
+    print(len(connection.queries))
+    for node in reversed(nodes):
         if node.link == path:
             active_node = node
-            break
+        if node.level == 0:
+            nodelists.append([node])
+            nodes.remove(node)
+    print(len(connection.queries))
 
-    menu = []
-    for node in nodes:
+    # TODO: Remove all references to node.parent, for it makes unwanted queries in numbers
+    while nodes:
+        for node in reversed(nodes):
+            for nodelist in nodelists:
+                if node.parent in nodelist:
+                    nodelist.append(node)
+                    nodes.remove(node)
+                    break
+            if node in nodes:
+                nodes.remove(node)  # if the node is somehow parentless but has a parent (??) it will not lead us to an
+                # endless loop
+    print(len(connection.queries))
+    for nodelist in nodelists:
+        nodes.extend(nodelist)
         # We use only nodes related to the active one, and if we don't have one, we have no need to expand anything
-        if not active_node:
-            if node.level > 0:
-                continue
-        else:
-            # TODO: This thing now will expand everything above the active node, including unrelated ones.
-            # TODO: Children must be put strictly after their parents.
-            if node.level > active_node.level:
-                if not node.parent == active_node:
-                    continue
+        # if not active_node:
+        #     if node.level > 0:
+        #         continue
+        # else:
+        #     # TODO: This thing now will expand everything above the active node, including unrelated ones.
+        #     # TODO: Children must be put strictly after their parents.
+        #     if node.level > active_node.level:
+        #         if not node.parent == active_node:
+        #             continue
 
+    for node in nodes:
         # Crude ASCII design
         name = node.name if not node.parent else '|--' * node.level + node.name
         menu.append({
@@ -34,5 +55,6 @@ def draw_menu(menu_name, path):
             'margin': node.level * 15,  # can be used instead of crude ASCII design
             'link': node.link,
         })
+    print(len(connection.queries))
 
     return {'menu': menu}
